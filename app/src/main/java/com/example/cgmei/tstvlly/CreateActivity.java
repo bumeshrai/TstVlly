6f7b6d689fa42c708cc7799cccabdfb2aa09359e
bumeshrai <bumeshrai@hotmail.com>
2016-06-17 18:27:43 +0530

    Volley JSON response solved

--- app/src/main/java/com/example/cgmei/tstvlly/CreateActivity.java
+++ app/src/main/java/com/example/cgmei/tstvlly/CreateActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
@@ -24,59 +26,55 @@ public class CreateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
+        final TextView tvMessage = (TextView) findViewById(R.id.tvMessage);
+        final TextView tvEngineer = (TextView) findViewById(R.id.tvEngineer);
+        final TextView tvContractor = (TextView) findViewById(R.id.tvContractor);
+
         Intent intent = getIntent();
         String parentString = intent.getStringExtra("parent");
 
         try {
             this.parent = new JSONObject(parentString);
+            parent.put("eng_id","6");
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        Log.i("value","At Create, Parent: "+parent);
-        //getResponse();
-        getResponseStr();
-    }
-
-    private void getResponse(){
+        //Log.i("value","At Create, Parent: "+parent);
         Response.Listener<JSONObject> responseListener = new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject jsonResponse) {
                 try {
-                    jsonResponse = jsonResponse.getJSONObject("data");
-                    Log.i("value", "Response: "+jsonResponse.toString());
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        // Volley Request
-        WorksheetRequest worksheetRequest = new WorksheetRequest(parent, responseListener);
-        RequestQueue queue = Volley.newRequestQueue(CreateActivity.this);
-        queue.add(worksheetRequest);
-        Log.i("here","Method Called");
-    }
-
-    private void getResponseStr(){
-        Response.Listener<String> responseListener = new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonResponse = new JSONObject(response);
                     //Log.i("value", "Response: "+jsonResponse.toString());
                     jsonResponse = jsonResponse.getJSONObject("data");
-                    Log.i("value", "Response: "+jsonResponse.toString());
+                    //Log.i("value", "Response: "+jsonResponse.toString());
+                    boolean success = jsonResponse.getBoolean("success");
+                    if(success){
+                        String message = jsonResponse.getString("message");
+                        String username = jsonResponse.getString("username");
+                        String organisation = jsonResponse.getString("organisation");
+
+                        tvMessage.setText(message);
+                        tvEngineer.setText("Engineer: "+username);
+                        tvContractor.setText("Contractor: "+organisation);
+                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         };
 
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Volley","Error");
+            }
+        };
+
         // Volley Request
-        TestClass worksheetRequest = new TestClass(parent.toString(), responseListener);
+        WorksheetRequest worksheetRequest = new WorksheetRequest(parent, responseListener, errorListener);
         RequestQueue queue = Volley.newRequestQueue(CreateActivity.this);
         queue.add(worksheetRequest);
-        Log.i("here","String Method Called");
-    }
+        //Log.i("here","Method Called");
 
+    }
 }
--- app/src/main/java/com/example/cgmei/tstvlly/WorksheetRequest.java
+++ app/src/main/java/com/example/cgmei/tstvlly/WorksheetRequest.java
@@ -16,8 +16,9 @@ public class WorksheetRequest extends JsonObjectRequest {
     private static final String LOGIN_REQUEST_URL
             = "http://cmrlvent.co.in/assetMaint/api/web/lift/create";
 
-    public WorksheetRequest (JSONObject parent,Response.Listener<JSONObject> responseListener){
-        super(Request.Method.POST, LOGIN_REQUEST_URL, (parent == null) ? null : parent, responseListener, null);
+    public WorksheetRequest (JSONObject parent,
+                             Response.Listener<JSONObject> responseListener, Response.ErrorListener errorListener){
+        super(Request.Method.POST, LOGIN_REQUEST_URL, parent, responseListener, errorListener);
         Log.i("here","Reached WSRequest");
     }
 }
